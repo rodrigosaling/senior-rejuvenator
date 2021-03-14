4939e441ed1f171ef1cfd6b5b502748ab74188a8
Rodrigo Saling <rodrigosaling@gmail.com>
2021-03-14 20:32:23 -0300

    Testing the content script

--- background.js
+++ background.js
@@ -4,3 +4,12 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
     console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+// chrome.runtime.onMessage.addListener((message, callback) => {
+//     if (message == "runContentScript"){
+//         chrome.scripting.executeScript({
+//             file: 'contentScript.js'
+//         });
+//     }
+// });
+
