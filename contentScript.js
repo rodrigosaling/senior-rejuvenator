969149bfef8d743d7d1079425987a0021341d984
Rodrigo Saling <rodrigosaling@gmail.com>
2021-03-14 22:19:26 -0300

    First working feature! Menu tree labels are now clickable, replicating the open/close feature

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,22 @@
-console.log('HERE');
+let isMenuLoaded = false;
+
+const menuFrame = parent.document.querySelector('[name=MENU]');
+
+setTimeout(function () {
+    console.log(menuFrame.contentDocument.querySelector('transstr').innerHTML);
+    if (menuFrame.contentDocument.querySelector('transstr').innerHTML === '<title>Ronda Web</title>') {
+        getAreas();
+    }
+}, 1000);
+
+function getAreas() {
+    const areas = menuFrame.contentDocument.querySelectorAll('.area');
+
+    areas.forEach((area) => {
+        console.log(area.getAttribute('href'))
+        const href = area.getAttribute('href');
+        if (href.includes('javascript:clickOnNode')) {
+            area.parentNode.nextSibling.querySelector('a').setAttribute('href', href);
+        }
+    })
+}
